d4f2733ceca88fc7b635517476bac4a2fead3827
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-16 01:18:30 +0600

    Separated controllers

--- src/main/java/com/sublime/controller/UserController.java
+++ src/main/java/com/sublime/controller/UserController.java
@@ -2,16 +2,18 @@ package com.sublime.controller;
 
 import java.security.Principal;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sublime.entity.Blog;
-import com.sublime.entity.User;
 import com.sublime.service.BlogService;
 import com.sublime.service.UserService;
 
@@ -24,48 +26,23 @@ public class UserController {
 	@Autowired
 	private BlogService blogService;
 	
-	@ModelAttribute("user")
-	public User constructUser(){
-		return new User();
-	}
-	
 	@ModelAttribute("blog")
 	public Blog constructBlog(){
 		return new Blog();
-	}
-	
-	@RequestMapping("/users")
-	public String users(Model model){
-		model.addAttribute("users", userService.findAll());
-		return "users";
-	}
-	
-	@RequestMapping("/users/{id}")
-	public String detail(Model model, @PathVariable int id){
-		model.addAttribute("user", userService.findOneWithBlogs(id));
-		return "user-detail";
-	}
-	
-	@RequestMapping("/register")
-	public String showRegister(){
-		return "user-register";
-	}
-	
-	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String doRegister(@ModelAttribute("user") User user){
-		userService.save(user);
-		return "redirect:/register.html?success=true";
 	}
 	
 	@RequestMapping("/account")
 	public String account(Model model, Principal principal){
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
-		return "user-detail";
+		return "account";
 	}
 	
 	@RequestMapping(value="/account", method=RequestMethod.POST)
-	public String addBlog(@ModelAttribute("blog") Blog blog, Principal principal){
+	public String addBlog(Model model,@Valid @ModelAttribute("blog") Blog blog, Principal principal, BindingResult result){
+		if(result.hasErrors()){
+			return account(model, principal);
+		}
 		String name = principal.getName();
 		blogService.save(blog, name);
 		return "redirect:/account.html";
@@ -78,11 +55,4 @@ public class UserController {
 		return "redirect:/account.html";
 	}
 	
-	@RequestMapping("/user/remove/{id}")
-	public String removeUser(@PathVariable int id){
-		userService.delete(id);
-		return "redirect:/users.html";
-	}
-	
-
 }
