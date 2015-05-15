61bb608d28decda51970891661b5cd764f66d7f8
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-16 01:58:22 +0600

    Annotation added for Unique user name

--- src/main/java/com/sublime/entity/User.java
+++ src/main/java/com/sublime/entity/User.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+import com.sublime.annotation.UniqueUsername;
+
 
 
 @Entity
@@ -26,6 +28,7 @@ public class User {
 	
 	@Size(min=3, message = "Name must be at lease 3 characters !" )
 	@Column(unique = true)
+	@UniqueUsername(message = "Such a username already exists!")
 	private String name;
 	
 	@Size(min=3, message = "Invalid email address!")
